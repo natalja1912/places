f1e90ec626cca24eb448f3aca52e8e23aaae31f5
Natalia Pavlova <natalja1912@yandex.ru>
2020-08-13 19:52:52 +0300

    feat: new pictures

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,23 +23,23 @@ export const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: 'Хибины',
+        link: 'https://a.d-cd.net/d213f6as-960.jpg'
     },
     {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: 'Ярославль',
+        link: 'https://photocentra.ru/images/main85/857640_main.jpg'
     },
     {
         name: 'Камчатка',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
     },
     {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: 'Адыгея',
+        link: 'https://avatars.mds.yandex.net/get-pdb/1222673/1416f0f2-13c0-4d52-9392-19e07c62c57b/s1200'
     },
     {
         name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        link: 'https://newbur.ru/upload/iblock/e18/e183a1f319b2a30d3a7564a23020790d.jpg'
     }
 ];
